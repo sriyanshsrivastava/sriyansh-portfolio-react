ba4a6d98f39ce1c2b43c3b9c669db89dfc24a64c
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-09-02 01:23:41 +0530

    add components navbar and footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,5 @@
+function Footer(){
+    return <h1>this is footer content</h1>;
+}
+
+export default Footer
---
+++ src/components/navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar(){
+    return(
+        <>
+            <h1 className="">Navbar</h1>
+        </>
+    );
+}
+
+export default Navbar
