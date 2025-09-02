d257a24a94af5ffccc6b6f38bc9a94d9ab051790
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-09-03 02:32:27 +0530

    fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,16 @@
 function Navbar(){
     const navlinks=["Home","About","Skills","Projects","Contact"]
     return(
-        <div className="flex items-center">
-            <div>Sriyansh S.</div>
-            <div>
-                <ul className="flex">
-                    {navlinks.map((link, index) => (
-                        <li key={index}>{link}</li>
-                    ))}
-                </ul>
+        <div className="flex bg-black items-center place-content-center h-15 fixed">
+            <div className="flex  px-5 place-content-between">
+                <div className="text-white">Sriyansh S.</div>
+                <div>
+                    <ul className="flex text-white gap-5">
+                        {navlinks.map((link, index) => (
+                            <li className="" key={index}>{link}</li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     );
